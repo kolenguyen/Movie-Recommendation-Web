ac7631c35dbbec770047debbe1f55eeb4608349e
prajwalashambulingappa <prajwalams029@gmail.com>
2024-03-13 20:30:51 -0400

    Added the landing page

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import 'Routes' instead of 'Switch'
+import Home from './pages/Home';
 import './App.css';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes> 
+          <Route exact path='/' element={<Home />} /> 
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
