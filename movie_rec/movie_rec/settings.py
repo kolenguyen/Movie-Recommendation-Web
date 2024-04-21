70d4d9303c14afa0726bffa7c94857923c944bc7
kole <khoa.fsu@gmail.com>
2024-04-21 19:27:07 -0400

    d5

--- movie_rec/movie_rec/settings.py
+++ movie_rec/movie_rec/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import django_heroku
 # import dotenv
-import dj_database_url
+# import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
